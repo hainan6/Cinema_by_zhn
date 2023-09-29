d591a125a95c516b686139c55cd9157df08cfc16
hainan6 <111573109+hainan6@users.noreply.github.com>
2023-09-29 08:37:13 +0800

    Delete cinema directory

--- cinema/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.sql.*;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        CinemaApp cinemaApp = new CinemaApp();
-        cinemaApp.cinemaApp();
-    }
-}
